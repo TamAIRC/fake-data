41b2841f38ebf021d1b81fac04db57431eec6d1f
Ngô ĐứcTâm <133946215+Moobbot2@users.noreply.github.com>
2024-01-29 15:24:26 +0700

    update fake question

--- src/fakedata_questions.py
+++ src/fakedata_questions.py
@@ -12,22 +12,21 @@ if not os.path.exists(directory):
 # Generate data using Faker
 fake = Faker('vi_VN')
 questions = []
+Answers = []
 
 for _ in range(500000):
     options = [fake.sentence() for _ in range(4)]
-    answer = random.choice(options)
     question_sentence = fake.sentence()
     print("question", _)
     question = {
         # Convert hash to string
-        "question": question_sentence,
+        "question_detail": question_sentence,
+        "answer_options": options,
+        "correct_answer": random.randint(1, 4),
         "major": fake.word(),
         "difficulty": round(random.uniform(1, 10), 0),
-        "type": "Trắc nghiệm",
         "rating": round(random.uniform(1, 5), 1),
-        "options": options,
         "reward": random.choice([10, 100, 200, 300, 500, 1000]),
-        "answer": answer
     }
     questions.append(question)
 # print (questions)
